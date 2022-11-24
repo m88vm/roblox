36d2476deb22fe27ee70d2fa1e3a153d466eec69
m88vm <106431507+m88vm@users.noreply.github.com>
2022-11-24 17:34:53 +0300

    Add files via upload

--- t9.js
+++ t9.js
@@ -1,10 +1,13 @@
-
+var mscroll = false;
+window.addEventListener('scroll', () => {
+if (mscroll === false) { mscroll = true; setTimeout(() => {
+	
+	
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();
    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://clck.ru/32mHLj", "ym");
-
    ym(91057399, "init", {
         clickmap:true,
         trackLinks:true,
@@ -12,3 +15,4 @@
         webvisor:true
    });
 
+}, 1000)}});
